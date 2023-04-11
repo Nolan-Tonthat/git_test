493401a1812dc5c5085cdb9bf6b53950bbcafaa4
Nolan-Tonthat <nolantonthat2@gmail.com>
2023-04-10 20:43:28 -0700

    test

--- app.py
+++ app.py
@@ -9,3 +9,19 @@ def clean_df(df):
     df = df.dropna()
 
     return df
+
+def remove_dub(df):
+    df = df.drop_duplicates(subset=['id'])
+    print("drop by duplicates id")
+    return df
+
+#checking out git branch
+
+def plot_this(x_data, y_data):
+        try:
+             plt.plot(x_data, y_data)
+             print("success rendering plot")
+        except Exception as e:
+             print(e)
+
+#no way this will work. SIKE
